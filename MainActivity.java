fe1c8df97b42bf32df4bae0444fd8132f8f63a6e
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2018-12-21 18:34:03 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,10 @@
 package com.hw.hw;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,24 +18,29 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     TextView textview;
-    private String provider;
-    private LocationManager my_lm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    /*--------------------------------------GPS module-----------------------------------------------*/
+    /*---GPS initialization---*/
+    LocationManager my_lm;
+    String provider;
+
+    //method when clicking the gps
+    public void getlocation(View view)
+    {
         List<String> list;
 
         textview = (TextView)findViewById(R.id.tv);
 
-        // acquire the localization service
-        my_lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-
         // acquire available localization device
         list = my_lm.getProviders(true);
         if(list.contains(LocationManager.GPS_PROVIDER)){
+            provider = LocationManager.GPS_PROVIDER;
             textview.append("GPS providing..."+"\n");
         }else
         {
@@ -55,26 +58,25 @@ public class MainActivity extends AppCompatActivity {
             string = "longitude: " + location.getLongitude() + "\n" + "latitude: " + location.getLatitude();
             Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
         }
-
-        LocationListener my_ll = new LocationListener(){
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.d(TAG, "onlocationchanged: ");
-                Log.d(TAG, "onlocationchanged: latitude = "+location.getLatitude());
-                Log.d(TAG, "onlocationchanged: longitude = "+location.getLongitude());
-                Log.d(TAG, "onlocationchanged: provider = "+location.getProvider());
-                textview.append("longitude: "+location.getLongitude()+"\n"+"latitude: "+location.getLatitude());
-            }
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras){}
-            @Override
-            public void onProviderEnabled(String provider){}
-            @Override
-            public void onProviderDisabled(String provider){}
-        };
-
-
-
-        my_lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,my_ll);
     }
+
+    LocationListener my_ll = new LocationListener(){
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(TAG, "onlocationchanged: ");
+            Log.d(TAG, "onlocationchanged: latitude = "+location.getLatitude());
+            Log.d(TAG, "onlocationchanged: longitude = "+location.getLongitude());
+            Log.d(TAG, "onlocationchanged: provider = "+location.getProvider());
+            textview.append("longitude: "+location.getLongitude()+"\n"+"latitude: "+location.getLatitude());
+        }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras){}
+        @Override
+        public void onProviderEnabled(String provider){}
+        @Override
+        public void onProviderDisabled(String provider){}
+    };
+
+    /*-------------------------------------Sensor module-----------------------------------------*/
+    /* ---------------------- Sensor data ------------------- */
 }
