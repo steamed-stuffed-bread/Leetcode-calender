cda74fabe4d3687c0592af22abefb4aecea41392
ctrlworks <probation2@ctrlworks.com>
2019-08-14 14:33:39 +0800

    2019-08-14

---
+++ 24_swap_nodes_in_pairs.py
@@ -0,0 +1,25 @@
+# medium
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        dummy = ListNode(-1)
+        dummy.next = head
+        pre = dummy
+        while pre.next and pre.next.next:
+            temp = pre.next.next
+            pre.next.next = temp.next
+            temp.next = pre.next
+            pre.next = temp
+            pre = pre.next.next
+        return dummy.next
