dc9ea40b5058a623f754192850092a5b397b23c0
ctrlworks <probation2@ctrlworks.com>
2019-08-22 13:28:45 +0800

    2019-08-22

--- 39_combination_sum.py
+++ 39_combination_sum.py
@@ -25,3 +25,29 @@ class Solution(object):
                 self.helper(target-self.can[i], i, out)
                 out.pop()
 
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        self.res = []
+        out = []
+        sorted(candidates)
+        self.can = candidates
+        self.helper(target, 0 ,out)
+        return self.res
+
+    def helper(self, target, left, out):
+        if target < 0:
+            return
+        elif target == 0:
+            self.res.append(out[:])
+            return
+        else:
+            for i in range(left, len(self.can)):
+                if self.can[i] > target:
+                    return
+                self.helper(target-self.can[i], i, out + [self.can[i]])
+
