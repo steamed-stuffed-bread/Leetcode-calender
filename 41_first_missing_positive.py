4ffb3e76ba6238174f9407138058f6257e085bed
ctrlworks <probation2@ctrlworks.com>
2019-08-23 10:02:24 +0800

    2019-08-23

---
+++ 41_first_missing_positive.py
@@ -0,0 +1,17 @@
+# hard
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        for i in range(n):
+            while nums[i] <= n and nums[i] > 0 and nums[nums[i]-1] != nums[i]:
+                temp = nums[i]
+                nums[i] = nums[nums[i]-1]
+                nums[temp-1] = temp
+        for i in range(n):
+            if nums[i] != i+1:
+                return i+1
+        return n+1
