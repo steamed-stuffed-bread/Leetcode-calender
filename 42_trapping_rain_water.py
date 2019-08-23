d443d8786a1efa67d172ecde44cec510990b56ca
ctrlworks <probation2@ctrlworks.com>
2019-08-23 11:05:54 +0800

    2019-08-23

--- 42_trapping_rain_water.py
+++ 42_trapping_rain_water.py
@@ -9,13 +9,16 @@ class Solution(object):
         res = 0
         dp = [0 for i in range(n)]
         mx = 0
+        # get the max possible height from left to right for every height
         for i in range(n):
             dp[i] = mx
             mx = max(mx, height[i])
         mx = 0
+        # get the max possible height from right to left for every height
         for i in range(n-1, -1, -1):
             dp[i] = min(mx, dp[i])
             mx = max(mx, height[i])
+            # the height is smaller than the possible max rain height, so the rain can be cumulated
             if dp[i] > height[i]:
                 res = res + (dp[i] - height[i])
         return res
