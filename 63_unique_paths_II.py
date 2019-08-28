b425bfacb0ac9b34d5fd3de7fbef17372d630b91
ctrlworks <probation2@ctrlworks.com>
2019-08-28 13:24:26 +0800

    2019-08-28

---
+++ 63_unique_paths_II.py
@@ -0,0 +1,16 @@
+# medium
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0 for i in range(n+1)] for j in range(m+1)]
+        dp[0][1] = 1
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if obstacleGrid[i-1][j-1] == 0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m][n]
