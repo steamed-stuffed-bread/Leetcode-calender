fbe2f00e619e21dfe7340e9d2d541f97ac436484
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-10-18 17:17:32 +0800

    Update 63_unique_paths_II.py

--- 63_unique_paths_II.py
+++ 63_unique_paths_II.py
@@ -14,3 +14,21 @@ class Solution(object):
                 if obstacleGrid[i-1][j-1] == 0:
                     dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m][n]
+
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        n = len(obstacleGrid)
+        m = len(obstacleGrid[0])
+        dp = [0 for i in range(m)]
+        dp[0] = 1
+        for i in range(n):
+            for j in range(m):
+                if obstacleGrid[i][j]:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] = dp[j] + dp[j-1]
+        return dp[m-1]
