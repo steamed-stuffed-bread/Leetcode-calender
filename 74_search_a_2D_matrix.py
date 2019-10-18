88345fce1048b15bbaba2f08f281ed23f8f7bdbf
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-10-18 15:38:33 +0800

    Update 74_search_a_2D_matrix.py

--- 74_search_a_2D_matrix.py
+++ 74_search_a_2D_matrix.py
@@ -20,3 +20,26 @@ class Solution(object):
             else:
                 j = j - 1
         return False
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+        row = len(matrix)
+        col = len(matrix[0])
+        left = 0
+        right = row*col-1
+        while left<=right:
+            mid = left + (right-left)/2
+            if matrix[mid/col][mid%col] == target:
+                return True
+            elif matrix[mid/col][mid%col] < target:
+                left = mid+1
+            else:
+                right = mid-1
+        return False
