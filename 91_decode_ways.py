fa8ee9527217b831672137970a483ef08fcaa6e4
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-10-18 18:13:47 +0800

    Update 91_decode_ways.py

--- 91_decode_ways.py
+++ 91_decode_ways.py
@@ -10,7 +10,7 @@ class Solution(object):
         dp = [0 for i in range(n+1)]
         dp[0] = 1
         for i in range(1, n+1):
-            dp[i] = dp[i-1] if s[i-1] != '0' else 0
+            dp[i] = dp[i-1] if s[i-1] != '0' else 0 # at least this position has the same number of division as the previous one
             if i > 1 and (s[i-2] == '1' or (s[i-2] == '2' and s[i-1] < '7')):
                 dp[i] = dp[i] + dp[i-2]
         return dp[n]
