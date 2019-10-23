070854f46b74c4b8c4457eb9aa0abf0f8945b8a2
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-10-23 15:33:34 +0800

    Update 169_majority_element.py

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -19,3 +19,22 @@ class Solution(object):
             if zero < one:
                 res = res | (1<<i)
         return res if res >> 31 == 0 else res-(1<<32)
+
+    
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return self.helper(nums, 0, len(nums)-1)
+    def helper(self,orgnum, left, right):
+        if left == right:
+            return orgnum[left]
+        mid = left + (right-left)/2
+        major_l = self.helper(orgnum, left, mid)
+        major_r = self.helper(orgnum, mid+1, right)
+        if major_l == major_r:
+            return major_l
+        else:
+            return major_l if orgnum[left:right+1].count(major_l) > orgnum[left:right+1].count(major_r) else major_r
