f44cd9c36a4bfb3b6166e902347ef7e421dd9463
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-11-24 13:54:44 +0800

    Update 114_flatten_binary_tree_to_linked_list.py

--- 114_flatten_binary_tree_to_linked_list.py
+++ 114_flatten_binary_tree_to_linked_list.py
@@ -12,20 +12,45 @@ class Solution(object):
         :type root: TreeNode
         :rtype: None Do not return anything, modify root in-place instead.
         """
-        if not root:
-            return
         self.res = []
         self.helper(root)
-        root = self.res.pop()
-        node = root
-        while len(self.res) != 0:
-            node.left = None
-            node.right = self.res.pop()
-            node = node.right
+        for i in range(1,len(self.res)):
+            self.res[i-1].left = None
+            self.res[i-1].right = self.res[i]
         
     def helper(self,root):
         if not root:
             return
-        self.res.insert(0, root)
-        self.helper(root.left)
-        self.helper(root.right)
+        self.res.append(root)
+        if root.left:
+            self.helper(root.left)
+        if root.right:
+            self.helper(root.right)
+            
+            
+            
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        if root.left:
+            self.flatten(root.left)
+        if root.right:
+            self.flatten(root.right)
+        tmp = root.right
+        root.right = root.left
+        root.left = None
+        while root.right:
+            root = root.right
+        root.right = tmp
