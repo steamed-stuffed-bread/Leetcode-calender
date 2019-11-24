ba5ff4b7b9e098a4b242930342a4f270b3f5de05
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-11-24 14:12:11 +0800

    Update 116_Populating_Next_Right_Pointers_in_Each_Node.py

--- 116_Populating_Next_Right_Pointers_in_Each_Node.py
+++ 116_Populating_Next_Right_Pointers_in_Each_Node.py
@@ -22,3 +22,36 @@ class Solution(object):
         self.connect(root.left)
         self.connect(root.right)
         return root
+
+    
+    
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if not root:
+            return None
+        q = [root]
+        while q:
+            n = len(q)
+            for i in range(n):
+                tmp = q[len(q)-1]
+                q.pop()
+                if i < n-1:
+                    tmp.next = q[len(q)-1]
+                if tmp.left:
+                    q.insert(0,tmp.left)
+                if tmp.right:
+                    q.insert(0,tmp.right)
+        return root
