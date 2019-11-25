1ffd1826e8e745ff46a639f598a528ee793ab57f
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-11-25 13:00:15 +0800

    Create 153_Find_Minimum_in_Rotated_Sorted_Array.py

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
+        while left < right:
+            mid = left + (right-left)/2
+            if nums[right] < nums[mid]:
+                left = mid+1
+            else:
+                right = mid
+        return nums[right]
