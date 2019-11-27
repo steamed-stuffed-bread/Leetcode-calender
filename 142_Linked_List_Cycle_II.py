3a064c5c48febf18a829ee4caae7ebb175f06222
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-11-27 12:47:33 +0800

    Update 142_Linked_List_Cycle_II.py

--- 142_Linked_List_Cycle_II.py
+++ 142_Linked_List_Cycle_II.py
@@ -1,3 +1,22 @@
+#     a
+# ---------*----
+#          |c    \ b
+#          |     /
+#           ----*
+
+# length of loop r
+# length of list l
+# s = (a+b)
+# vfast = 2vslow
+# 2s = s + nr
+# a+b = nr
+# a+b = (n-1)r + l-a
+# a = (n-1)r + l-a-b
+# a = c + (n-1)r
+# next time fast start from meeting point, slow start from start point
+# before they meet at the start point of loop, fast may have round the loop several times.
+# but they are meant to meet at the start point of the loop.
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
