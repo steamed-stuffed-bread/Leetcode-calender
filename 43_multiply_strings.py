5320b9034f878c196926c3d3ed770dd9261f4f5c
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2019-12-01 16:41:34 +0800

    Update 43_multiply_strings.py

--- 43_multiply_strings.py
+++ 43_multiply_strings.py
@@ -30,3 +30,28 @@ class Solution(object):
             res.append(chr(tmp[i] + ord("0")))
             i = i-1
         return ''.join(res)
+
+    
+    class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        m = len(num1)
+        n = len(num2)
+        val = [0 for i in range(m+n)]
+        res = ""
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                mul = ord(num1[i])-ord('0')
+                b_mul = ord(num2[j])-ord('0')
+                tmp = mul*b_mul
+                s = tmp+val[i+j+1]
+                val[i+j] = val[i+j] + s/10
+                val[i+j+1] = s%10
+        for e in val:
+            if not (len(res)==0 and e==0):
+                res = res + chr(e+ord('0'))
+        return res if len(res)!=0 else "0"
