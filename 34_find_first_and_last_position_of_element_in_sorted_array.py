5d388a7c9a3119499a0adf8ebe07a177231041a5
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2020-07-14 13:45:10 +0800

    Update 34_find_first_and_last_position_of_element_in_sorted_array.py

--- 34_find_first_and_last_position_of_element_in_sorted_array.py
+++ 34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -61,3 +61,31 @@ class Solution(object):
                 right = mid-1
         res[1] = right
         return res
+    
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2,-1);
+        int left = 0, right = nums.size();
+        int mid = 0;
+        while(left < right) {
+            mid = left + (right-left)/2;
+            if (nums[mid] < target)   /* find first one equal or larger */
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        if (right == nums.size() || nums[right] != target) return res;
+        res[0] = right;
+        right = nums.size();
+        while(left < right) {
+            mid = left + (right-left)/2;
+            if (nums[mid] <= target)  /* find first one larger */
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        res[1] = right-1;
+        return res;
+    }
+};
