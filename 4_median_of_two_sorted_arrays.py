beee2378b15f58ca6a049ba3b6b638c246eabac0
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2020-08-28 11:12:34 +0800

    Update 4_median_of_two_sorted_arrays.py

--- 4_median_of_two_sorted_arrays.py
+++ 4_median_of_two_sorted_arrays.py
@@ -28,3 +28,23 @@ class Solution(object):
             return self.findkth(i+k/2, j, k-k/2)
         else:
             return self.findkth(i, j+k/2, k-k/2)
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size(), left = (m+n+1)/2, right = (m+n+2)/2;
+        return (helper(nums1,0,nums2,0,left) + helper(nums1,0,nums2,0,right))/2.0;
+    }
+    int helper(vector<int>& nums1, int l, vector<int>& nums2, int r, int p) {
+        if (l >= nums1.size()) return nums2[r+p-1];    // it is better to use >=, because size is unsigned int
+        if (r >= nums2.size()) return nums1[l+p-1];
+        if (p == 1) return min(nums1[l], nums2[r]);
+        
+        int mid1 = (l+p/2-1 >= nums1.size()) ? INT_MAX : nums1[l+p/2-1];
+        int mid2 = (r+p/2-1 >= nums2.size()) ? INT_MAX : nums2[r+p/2-1];
+        if (mid1 > mid2)
+            return helper(nums1, l, nums2, r+p/2, p-p/2);
+        else
+            return helper(nums1, l+p/2, nums2, r, p-p/2);
+    }
+};
