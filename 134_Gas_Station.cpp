43eac3ce48b03c8e20dd1b778775c3e8cdbf4a81
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2020-09-21 09:45:12 +0800

    Create 134_Gas_Station.cpp

---
+++ 134_Gas_Station.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total = 0, sum = 0, start = 0;
+        int n = gas.size();
+        for (int i = 0; i < n; ++i) {
+            total += gas[i] - cost[i];
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                sum = 0;
+                start = i+1;
+            }
+        }
+        return total < 0 ? -1:start;
+    }
+};
