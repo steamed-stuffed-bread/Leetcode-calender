8add7799554f5388f658201ecddc3eb9cc413e72
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2020-12-12 10:57:42 +0800

    Update 322_Coin_Change.cpp

--- 322_Coin_Change.cpp
+++ 322_Coin_Change.cpp
@@ -14,3 +14,27 @@ public:
         return dp[amount] > amount?-1:dp[amount];
     }
 };
+
+// Brute Force
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size(), res = INT_MAX;
+        sort(coins.begin(), coins.end());
+        helper(coins, amount, n-1, 0, res);
+        return (res == INT_MAX)?-1:res;
+    }
+    
+    void helper(vector<int>& coins, int t, int start, int cur, int& res) {
+        if (start < 0) return;
+        if (t%coins[start] == 0) {
+            res = min(res, cur + t/coins[start]);
+            return;
+        }
+        
+        for (int i = t/coins[start]; i >= 0; --i) {
+            if (cur + i >= res -1) break;
+            helper(coins, t-coins[start]*i, start-1, cur+i, res);
+        }
+    }
+};
