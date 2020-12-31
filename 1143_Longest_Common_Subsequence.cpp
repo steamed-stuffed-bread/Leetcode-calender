d8cf17624bde58db5ad3fe492d0f5302f1f1809c
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2020-12-31 12:15:01 +0800

    Create 1143_Longest_Common_Subsequence.cpp

---
+++ 1143_Longest_Common_Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        int i = 0, j = 0;
+        for (i = 1; i <= n1; ++i) {
+            for (j = 1; j <= n2; ++j) {
+                if (text1[i-1] == text2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+};
