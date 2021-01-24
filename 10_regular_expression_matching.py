dfd13052b526a294e33939949ee8c0fc8b7d8a93
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2021-01-24 10:28:56 +0800

    Update 10_regular_expression_matching.py

--- 10_regular_expression_matching.py
+++ 10_regular_expression_matching.py
@@ -13,7 +13,7 @@ class Solution(object):
         for i in range(m+1):
             for j in range(1, n+1):
                 if j > 1 and p[j-1] == '*':
-                    dp[i][j] = dp[i][j-2] or (i>0 and (s[i-1] == p[j-2] or p[j-2] == '.') and dp[i-1][j])
+                    dp[i][j] = dp[i][j-2] or (i>0 and (s[i-1] == p[j-2] or p[j-2] == '.') and dp[i-1][j]) // i-1 j is not about matching s i and p j-1, * only means that the previous repeating
                 else:
                     dp[i][j] = i>0 and dp[i-1][j-1] and (s[i - 1] == p[j - 1] or p[j - 1] == '.')
         return dp[m][n]
