5c757fac0c95f69078fff414985a904a4632a350
steamed-stuffed-bread <34907392+steamed-stuffed-bread@users.noreply.github.com>
2021-08-28 10:47:13 +0800

    Update 522_Longest_Uncommon_SubsequenceII.cpp

--- 522_Longest_Uncommon_SubsequenceII.cpp
+++ 522_Longest_Uncommon_SubsequenceII.cpp
@@ -26,3 +26,36 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    bool isSubseq(string s1, string s2) {
+        int i = 0, j = 0, len = 0;
+        for (i = 0; i < s2.size() && j < s1.size(); ++i) {
+            if (s1[j] == s2[i]) ++j;
+        }
+        return j == s1.size();
+    }
+    
+    int findLUSlength(vector<string>& strs) {
+        sort(strs.begin(), strs.end(), [](string s1, string s2){return s2.size() < s1.size();});
+        for (auto s:strs) {
+            cout << s << endl;
+        }
+        int res = -1;
+        int i = 0, j = 0;
+        for (i = 0; i < strs.size(); ++i)  {
+            bool flag = true;
+            for (j = 0; j < strs.size(); ++j) {
+                if (i == j) continue;
+                if (isSubseq(strs[i], strs[j])) {
+                    flag = false;
+                    break;
+                }
+            }
+            //if (j == strs.size()) res = max(res, (int)strs[i].size());
+            if (flag) return strs[i].size();
+        }
+        return res;
+    }
+};
